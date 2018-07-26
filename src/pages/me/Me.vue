a09030f38e0ad28bde2faf6d2b06001af2d00c57
shiyanping <shiyanping@coohua.com>
2018-07-26 21:42:47 +0800

    添加stylus

--- src/pages/me/Me.vue
+++ src/pages/me/Me.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
     这是我的页面
+    <div class="image"></div>
     <button open-type="getUserInfo" lang="zh_CN" @getuserinfo="onGotUserInfo">获取用户信息</button>
     <button @click="handleScanCode">调起扫码</button>
   </div>
 </template>
 
 <script>
-import config from '@/utils/config';
-import request from '@/utils/request';
-import utils from '@/utils/utils';
+import config from '@/http/api';
+import request from '@/http/request';
+import utils from '@/utils/index';
 export default {
   async created() {
     console.log(utils.dateFormat(new Date(), 'yyyy-MM-dd'));
     const res = await request(config.host, {}, 'GET');
     console.log(res);
+  },
+  mounted() {
+    console.log(this.$root.$mp.appOptions);
   },
   methods: {
     onGotUserInfo(e) {
@@ -31,5 +35,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
+.image
+  width 20px
 </style>
